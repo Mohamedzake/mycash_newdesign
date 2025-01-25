47bd49e016ee92c858ec325c92ca86871fe0b268
Mohamedzake <zakem7671@gmail.com>
2025-01-25 18:37:12 +0200

    J

--- app/reachmycash/page.tsx
+++ app/reachmycash/page.tsx
@@ -2,9 +2,6 @@
 import React from "react";
 import Header from "../_components/Header";
 
-import OurPartners from "../_components/OurPartners";
-import OurPartners1 from "../_components/OurPartners1";
-import OurPartners2 from "../_components/OurPartners2";
 import WhyMyCash from "../_components/WhyMyCash";
 import LandingForm from "../_components/LandingForm";
 import WhyChooseMyCash from "../_components/WhyChooseMyCash";
